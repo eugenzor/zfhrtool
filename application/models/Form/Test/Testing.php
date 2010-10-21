a0482edaab07af2af8f621765156a00be4e77f85
misha.lintur <misha.lintur@gmail.com>
2010-10-21 14:14:00 +0000

    Добавлена подсветка кода в тестах.

--- application/models/Form/Test/Testing.php
+++ application/models/Form/Test/Testing.php
@@ -33,9 +33,35 @@ class Form_Test_Testing extends Zend_Form {
      * @return void
      */
     public function addElementsForm(array $questions, array $answers) {
+        $tags = '(?:php)|(?:sql)|(?:css)|(?:js)|(?:html)|(?:bash)';
+        $classExistsTH = class_exists('Text_Highlighter', false);
         foreach ($questions as $id => $question) {
+            $text = $question['tq_text'];
+            $text = nl2br(htmlspecialchars($text));
+            
+            if ($classExistsTH){
+                $text .= '[code lang=""][/code]';
+                // заменяет все теги на [code lang="langName"] и [/code]
+                $text = preg_replace('/\[('.$tags.')\](.*?)\[\/(?:'.$tags.')\]/i', "[code lang='$1']$2[/code]", $text);
+                //розбтвает строку и записывает в массив
+                preg_match_all("/(.*?)\[code lang=['\"](.*?)['\"]\](.*?)\[\/code\]/", $text, $matches);
+                $text = '';
+                $count = count($matches[0]);
+                for ($i=0; $i<$count; $i++){
+                    $text .= $matches[1][$i];
+                    if($matches[2][$i]){
+                        $hl = &Text_Highlighter::factory($matches[2][$i]);
+                        $text .= $hl->highlight($matches[3][$i]);
+                    }
+                }
+            } else {
+                $text = preg_replace('/(\[code lang=.*?\])/i', '<pre>', $text);
+                $text = str_replace(array('[php]', '[sql]','[css]', '[js]', '[html]', '[bash]'),'<pre>',$text);
+                $text = str_replace(array('[/php]', '[/sql]', '[/css]', '[/js]', '[/html]', '[/bash]', '[/code]'),'</pre>',$text);
+            }
+
             $elemQuestion = $this->createElement('hidden', 'question_' . $id)
-                            ->setDescription($question['tq_sort_index'] . '. ' . nl2br(htmlspecialchars($question['tq_text'])));
+                            ->setDescription($question['tq_sort_index'] . '. ' . $text);
             $elemQuestion->getDecorator('Description')->setOption('escape', false);
             $this->addElement($elemQuestion);           
 
